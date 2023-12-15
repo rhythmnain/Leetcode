58d6bdd011747830af29093a72af41ee83e58aed
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2023-12-16 01:45:31 +0530

    Time: 2 ms (83.05%) | Memory: 40.1 MB (95.51%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // No matching opening bracket
+                }
+
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false; // Mismatched opening and closing brackets
+                }
+            }
+        }
+
+        return stack.isEmpty(); // Check if there are unmatched opening brackets
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Test cases
+        System.out.println(solution.isValid("()"));       // true
+        System.out.println(solution.isValid("()[]{}"));   // true
+        System.out.println(solution.isValid("(]"));       // false
+        System.out.println(solution.isValid("([)]"));     // false
+        System.out.println(solution.isValid("{[]}"));     // true
+    }
+}
