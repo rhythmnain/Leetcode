c2888ecb05376f168731af53551b788e52d4af17
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-04 16:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (83.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,29 +1,53 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int start = 0;
+        int end = nums.length-1;
+        while(start<= end){
+            int mid = (start+end)/2;
 
-            if (nums[mid] == target) {
+            if(nums[mid]==target){
                 return mid;
             }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<= target && target<nums[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            } else{
+                if(nums[mid]<target && target<= nums[end]){
+                    start = mid+1;
+                } else{
+                    end =mid -1;
                 }
             }
         }
-
         return -1;
+        // int low = 0, high = nums.length - 1;
+
+        // while (low <= high) {
+        //     int mid = (low + high) / 2;
+
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     }
+
+        //     if (nums[low] <= nums[mid]) {
+        //         if (nums[low] <= target && target < nums[mid]) {
+        //             high = mid - 1;
+        //         } else {
+        //             low = mid + 1;
+        //         }
+        //     } else {
+        //         if (nums[mid] < target && target <= nums[high]) {
+        //             low = mid + 1;
+        //         } else {
+        //             high = mid - 1;
+        //         }
+        //     }
+        // }
+
+        // return -1;
     }
 }
