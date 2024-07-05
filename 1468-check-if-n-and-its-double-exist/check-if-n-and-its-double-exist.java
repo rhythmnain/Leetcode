4b2a0584b5f7a081683bc7e902e0615e0a71d6e0
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-05 14:47:56 +0530

    Time: 2 ms (94.77%) | Memory: 42.8 MB (44.76%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,23 +1,36 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        int n = arr.length;
-        
-        for(int i=0; i<n; i++){
-            int start= 0;
-            int end = n-1;
-            int mid;
-            while(start<=end){
-                mid = start+(end-start)/2;
-                if(arr[i]*2==arr[mid] && i!= mid){
-                    return true;
-                } else if(arr[i]*2>arr[mid]){
-                    start = mid+1;
-                } else{
-                    end = mid-1;
-                }
+
+        for(int i=0;i<arr.length;i++){
+            
+            for(int j=0;j<arr.length;j++){
+            if(arr[i] == 2*arr[j] && i!=j){
+                return true;
             }
+            }
+            
         }
         return false;
     }
 }
+//         // Arrays.sort(arr);
+//         // int n = arr.length;
+        
+//         // for(int i=0; i<n; i++){
+//         //     int start= 0;
+//         //     int end = n-1;
+//         //     int mid;
+//         //     while(start<=end){
+//         //         mid = start+(end-start)/2;
+//         //         if(arr[i]*2==arr[mid] && i!= mid){
+//         //             return true;
+//         //         } else if(arr[i]*2>arr[mid]){
+//         //             start = mid+1;
+//         //         } else{
+//         //             end = mid-1;
+//         //         }
+//         //     }
+//         // }
+//         // return false;
+//     }
+// }
