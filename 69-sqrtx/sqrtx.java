f3c5d08ec0ebe4aacb4540e1c876e481076951bf
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-05 10:07:36 +0530

    Time: 1 ms (86.85%) | Memory: 40.8 MB (27.68%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,29 +1,66 @@
 class Solution {
     public int mySqrt(int x) {
-      
-        if (x == 0 || x == 1)
+        if (x == 0 || x == 1) {
             return x;
-
+        }
         
         int start = 1;
         int end = x;
-        int mid = -1;
-
+        int ans = 0;
         
         while (start <= end) {
+            int mid = start + (end - start) / 2;
             
-            mid = start + (end - start) / 2;
-
-            
-            if ((long) mid * mid > (long) x)
-                end = mid - 1;
-            else if (mid * mid == x)
-                
-                return mid;
-            else
-                
+            if (mid <= x / mid) {
+                ans = mid;
                 start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-            return Math.round(end);
+        
+        return ans;
     }
 }
+
+// class Solution {
+//     public int mySqrt(int x, int[] arr) {
+      
+//         // if (x == 0 || x == 1)
+//         //     return x;
+//         // int start = 1;
+//         // int end = x-1;
+                
+//         // while (start <= end) {
+//         //    int mid = start + (end - start) / 2;
+//         //     if ((long) mid * mid > (long) x)
+//         //         end = mid - 1;
+//         //     else if (mid * mid == x)    
+//         //         return mid;
+//         //     else      
+//         //         start = mid + 1;
+//         // }
+//         //     return Math.round(end);
+
+//         if(x==0||x==1){
+//             return x;
+//         }
+//         int start = 0;
+//         int end = arr.length-1;
+
+//         while(start<= end){
+//             int mid = start+ (end-start)/2;
+
+//             if(x<arr[mid]){
+//                 end = mid -1;
+//             }
+//             else if(x>arr[mid]){
+//                 start = mid+1;
+
+//             } else {
+//                 return mid;
+//             }
+
+//         } return Math.round(end);
+//     }
+// }
