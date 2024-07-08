e8c5d6ef6dd08f368182a7ada3133606efb9778a
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-08 11:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (21.53%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,27 +1,54 @@
 class Solution {
     public int findMin(int[] nums) {
-        int n = nums.length;
-		int left = 0;
-		int right = n - 1;
-		int mid = 0;
-		int target = nums[left];
-		while (left <= right) {
-			mid = (left + right) / 2;
-			if (nums[mid] < target) {
-				target = nums[mid];
-			} else if (nums[left] < nums[mid]) {
-				// left is sorted
-				if(nums[mid] < nums[right])
-					right = mid - 1;
-				else
-					left = mid + 1;
-			} else {
-				if(nums[mid] < nums[right])
-					right = mid - 1;
-				else
-					left = mid + 1;
-			}
-		}
-		return target;
+        int start=0;
+        int end = nums.length-1;
+        int mid;
+        int ans = nums[start];
+
+        while(start<=end){
+            mid = start+(end-start)/2;
+            if (nums[mid] < ans) {
+                ans = nums[mid];
+            } else if(nums[start]<nums[mid]){
+                if(nums[mid]<nums[end]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            } else{
+                if(nums[mid]<nums[end]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+
+        }
+        return ans;
     }
 }
+//         int n = nums.length;
+// 		int left = 0;
+// 		int right = n - 1;
+// 		int mid = 0;
+// 		int target = nums[left];
+// 		while (left <= right) {
+// 			mid = (left + right) / 2;
+// 			if (nums[mid] < target) {
+// 				target = nums[mid];
+// 			} else if (nums[left] < nums[mid]) {
+// 				// left is sorted
+// 				if(nums[mid] < nums[right])
+// 					right = mid - 1;
+// 				else
+// 					left = mid + 1;
+// 			} else {
+// 				if(nums[mid] < nums[right])
+// 					right = mid - 1;
+// 				else
+// 					left = mid + 1;
+// 			}
+// 		}
+// 		return target;
+//     }
+// }
