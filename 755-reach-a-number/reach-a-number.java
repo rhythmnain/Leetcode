c3db2af90ae577ec91c657452357190cdb8ee1f2
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-08 13:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (57.82%) - LeetSync

--- 755-reach-a-number/reach-a-number.java
+++ 755-reach-a-number/reach-a-number.java
@@ -1,13 +1,37 @@
 class Solution {
     public int reachNumber(int target) {
-        target= Math.abs(target);
-        int sum=0;
-        int steps=0;
-
-        while(sum<target || (sum-target)%2!=0){
+        target=Math.abs(target);
+        int start=1,end=target,mid,steps=0,pos=0;
+        long sum=0;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            sum=(long)mid*(mid+1)/2;
+            if(sum>=target)
+                {
+                    pos=(int)sum;
+                    steps=mid;
+                    end=mid-1;
+                }
+            else
+                start=mid+1;
+        }
+        while((pos-target)%2!=0)
+        {
             steps++;
-            sum+=steps;
+            pos+=steps;
         }
         return steps;
     }
 }
+        // target= Math.abs(target);
+        // int sum=0;
+        // int steps=0;
+
+        // while(sum<target || (sum-target)%2!=0){
+        //     steps++;
+        //     sum+=steps;
+        // }
+        // return steps;
+//     }
+// }
