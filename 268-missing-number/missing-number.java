5cb6721228bbb881dcd0eddcdc40f540704dbedd
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-10 11:58:55 +0530

    Time: 1 ms (40.26%) | Memory: 45.1 MB (59.29%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,36 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int[] v = new int[n+1];
-        Arrays.fill(v, -1);
-        for(int i = 0; i < nums.length; i++) {
-            v[nums[i]] = nums[i];
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i];
+            if(nums[i]<nums.length && nums[i]!= nums[correct]){
+                swap(nums, i, correct);
+            } else{
+                i++;
+            }
         }
-        for(int i = 0; i < v.length; i++) {
-            if(v[i] == -1) return i;
+        for(int idx= 0; idx<nums.length; idx++){
+            if(nums[idx]!= idx){
+                return idx;
+            } 
         }
-        return 0;
+        return nums.length;
     }
+
+
+        // int n = nums.length;
+        // int[] v = new int[n+1];
+        // Arrays.fill(v, -1);
+        // for(int i = 0; i < nums.length; i++) {
+        //     v[nums[i]] = nums[i];
+        // }
+        // for(int i = 0; i < v.length; i++) {
+        //     if(v[i] == -1) return i;
+        // }
+        // return 0;
+     public static void swap(int[] nums, int first, int second){
+            int temp = nums[first];
+            nums[first]=nums[second];
+            nums[second]= temp;
+        }
 }
