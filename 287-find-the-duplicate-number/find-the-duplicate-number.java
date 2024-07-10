d7e06b29f4da2b00304125d31b466764775fb261
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-10 14:53:29 +0530

    Time: 6 ms (51.12%) | Memory: 58.1 MB (57.27%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,17 +1,37 @@
 class Solution {
   public int findDuplicate(int[] nums) {
-    int slow = nums[0];
-    int fast = nums[0];
-    do {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
-    } while (slow != fast);
+    int i =0;
+    while(i<nums.length){
 
-    fast = nums[0];
-    while (slow != fast) {
-      slow = nums[slow];
-      fast = nums[fast];
+        if(nums[i]!= i+1){
+            int correct= nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums, i, correct);
+            } else{
+                return nums[i];
+            }
+        }else{
+            i++;
+        }
     }
-    return slow;
+    return 0;
   }
-}
+static void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a]= nums[b];
+        nums[b]= temp;
+    }
+    // int slow = nums[0];
+    // int fast = nums[0];
+    // do {
+    //   slow = nums[slow];
+    //   fast = nums[nums[fast]];
+    // } while (slow != fast);
+
+    // fast = nums[0];
+    // while (slow != fast) {
+    //   slow = nums[slow];
+    //   fast = nums[fast];
+    // }
+    // return slow;
+  }
