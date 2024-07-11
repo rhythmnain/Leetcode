d18ec41f38cd8e37319a5a6d6a801676b513e065
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-11 11:20:30 +0530

    Time: 3 ms (79.44%) | Memory: 45.2 MB (83.33%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,33 +1,24 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int[] findErrorNums(int[] nums) {
-        Set<Integer> numSet = new HashSet<>();
-        int duplicate = -1, missing = -1;
-
-        for (int num : nums) {
-            if (!numSet.add(num)) {
-                duplicate = num;
+        int i =0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!= nums[correct]){
+                swap(nums, i, correct);
+            }else{
+                i++;
             }
         }
-
-        int expectedSum = (nums.length * (nums.length + 1)) / 2;
-        int actualSum = Arrays.stream(nums).sum();
-
-        missing = expectedSum - (actualSum - duplicate);
-
-        return new int[]{duplicate, missing};
+        for(int idx=0; idx<nums.length; idx++){
+            if(nums[idx]!= idx+1){
+                return new int[]{nums[idx], idx+1};
+            } 
+        }
+        return new int[]{-1,-1};
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums1 = {1, 2, 2, 4};
-        int[] result1 = solution.findErrorNums(nums1);
-        System.out.println(Arrays.toString(result1));  // Output: [2, 3]
-
-        int[] nums2 = {1, 1};
-        int[] result2 = solution.findErrorNums(nums2);
-        System.out.println(Arrays.toString(result2));  // Output: [1, 2]
+    public static void swap(int[] nums, int a, int b){
+        int temp= nums[a];
+        nums[a]= nums[b];
+        nums[b]= temp;
     }
 }
