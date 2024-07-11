b3cfb080323f6ee9d02468f10888eda7e24daca4
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-11 11:45:20 +0530

    Time: 1 ms (100.00%) | Memory: 57.1 MB (62.68%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,17 +1,40 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int n=nums.length;
-        for(int num:nums){
-            if(num>0){
-                set.add(num);
-            }
+        int i =0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+        if(nums[i]>0 && nums[i]<nums.length && nums[i]!= nums[correct]){
+            swap(nums, i, correct);
+        }else{
+            i++;
         }
-        for(int i=1;i<=n+1;i++){
-            if(!set.contains(i)){
-                return i;
-            }
         }
-        return -1;
+    
+    for(int idx=0; idx<nums.length; idx++){
+        if(nums[idx]!= idx+1){
+            return idx+1;
+        }
+    }
+    return nums.length+1;
+    }
+    public static void swap(int[] nums, int a , int b){
+        int temp = nums[a];
+        nums[a]= nums[b];
+        nums[b]= temp;
     }
 }
+        // HashSet<Integer> set=new HashSet<>();
+        // int n=nums.length;
+        // for(int num:nums){
+        //     if(num>0){
+        //         set.add(num);
+        //     }
+        // }
+        // for(int i=1;i<=n+1;i++){
+        //     if(!set.contains(i)){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+//     }
+// }
