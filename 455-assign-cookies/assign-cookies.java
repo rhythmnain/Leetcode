08504d2d2924b14403e4ac0cf2eea6cd902d510e
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-12 11:50:00 +0530

    Time: 8 ms (98.07%) | Memory: 45.4 MB (21.52%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
-        int contentChildren = 0;
-        int i = 0; // Pointer for g
-        int j = 0; // Pointer for s
-
-        while (i < g.length && j < s.length) {
-            if (s[j] >= g[i]) {
-                contentChildren++;
+        int i=0; 
+        int j=0;
+        int goodChild=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                goodChild++;
                 i++;
             }
             j++;
         }
-
-        return contentChildren;
+        return goodChild;
     }
 }
