f6eda7df95d2101a75aab3e704e07bec308816bc
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-12 14:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (71.17%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,41 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0 , high = n-1;
+        int start =0; 
+        int end = nums.length-1;
+        int mid;
 
-        while(low <= high){
-            int mid = (low + high)/2;
-
-            if(nums[mid] == target) return true;
-            
-            //this is the main condition
-            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
-                low = low + 1;
-                high = high -1;
-                continue;
-            }
-
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid] )
-                    high  = mid - 1;
-                else
-                    low = mid + 1;
-            }else{
-                if(nums[mid+1] <= target && target <= nums[high])
-                    low = mid + 1;
-                else
-                    high = mid-1; 
-            }
+while(start<=end){
+    mid = start+(end-start)/2;
+    if(nums[mid]== target){
+        return true;
+    }
+         if(nums[start]==nums[mid]&& nums[mid]== nums[end]){
+            start = start+1;
+            end = end -1;
         }
+    else if(nums[start]<=nums[mid]){
+        if(nums[start]<=target && target <=nums[mid]){
+            end = mid-1;
+        } else{
+            start = mid+1;
+        }
+    } else{
+        if(nums[mid+1]<=target && target<= nums[end]){
+            start = mid+1;
+        } else{
+            end = mid-1;
+        }
+    }
+}
+return false;
+        //     }else{
+        //         if(nums[mid+1] <= target && target <= nums[high])
+        //             low = mid + 1;
+        //         else
+        //             high = mid-1; 
+        //     }
+        // }
 
-        return false;
+        // return false;
     }
 }
