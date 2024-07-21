0b02a3bd730689ccaeb03458592dc80913933297
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-21 19:10:25 +0530

    Time: 3 ms (89.80%) | Memory: 41.5 MB (67.70%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,32 +1,19 @@
-public class Solution {
-    public static boolean halvesAreAlike(String s) {
-        int length = s.length();
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+        int count1 =0;
+        int count2 = 0;
         String vowels = "aeiouAEIOU";
-
-        // Count vowels in the first half
-        int countA = countVowels(s.substring(0, length / 2), vowels);
-
-        // Count vowels in the second half
-        int countB = countVowels(s.substring(length / 2), vowels);
-
-        // Check if the counts are equal
-        return countA == countB;
-    }
-
-    // Helper function to count vowels in a given string
-    private static int countVowels(String str, String vowels) {
-        int count = 0;
-        for (char ch : str.toCharArray()) {
-            if (vowels.indexOf(ch) != -1) {
-                count++;
-            }
+        for(int i=0; i<n/2; i++){
+            char ch = s.charAt(i);
+            if (vowels.indexOf(ch) != -1)
+            count1++;
+        } 
+        for(int i=n/2; i<n; i++){
+            char ch = s.charAt(i);
+            if (vowels.indexOf(ch) != -1)
+            count2++;
         }
-        return count;
-    }
-
-    public static void main(String[] args) {
-        // Test cases
-        System.out.println(halvesAreAlike("book"));      // Output: true
-        System.out.println(halvesAreAlike("textbook"));  // Output: false
+        return count1==count2;
     }
 }
