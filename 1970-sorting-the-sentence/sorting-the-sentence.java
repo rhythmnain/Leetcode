5b1a737e9bae9175d18b6ae6b1ba62d512cd67ee
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-22 06:47:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (53.15%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -3,10 +3,11 @@ class Solution {
         String[] arr = s.split(" ");
         String[] ans = new String[arr.length];
         int val;
-        for(String i:arr){
+
+        for(String i : arr){
             val = i.charAt(i.length()-1)-'0'-1;
-            ans[val]= i.substring(0, i.length()-1);
-        } 
+            ans[val]=i.substring(0,i.length()-1);
+        }
         StringBuilder sb = new StringBuilder();
         for(String i:ans){
             sb.append(i);
