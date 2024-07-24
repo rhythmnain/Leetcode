229a1b36a0bdceb96e01c3b1f95476d9f7ea84a8
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-24 11:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (8.93%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String ss[] = s.split(" ");
-        return ss[ss.length-1].length();
+        int n = s.length();
+        int m = 0;
+        boolean found = false;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') { 
+                m++;
+                found = true;
+            } else if (found) {
+                break;
+            }
+        }
+        return m;
     }
 }
