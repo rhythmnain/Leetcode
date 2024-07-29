01fc1c92e7185592b20eb34222d6e4e48d5c31fd
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-07-29 21:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (98.50%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,33 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Integer> spiralOrder(int[][] m) {
-        int l=0,r=m[0].length-1,u=0,d=m.length-1;
-        List<Integer> ll=new ArrayList<>();
-        while(true){
-            if(l<=r){
-                for(int i=l;i<=r;++i){
-                    ll.add(m[u][i]);
-                }
-                u++;
-            }else break;
-            if(u<=d){
-                for(int i=u;i<=d;++i){
-                    ll.add(m[i][r]);
-                }
-                r--;
-            }else break;
-            if(l<=r){
-                for(int i=r;i>=l;--i){
-                    ll.add(m[d][i]);
-                }
-                d--;
-            }else break;
-            if(u<=d){
-                for(int i=d;i>=u;--i){
-                    ll.add(m[i][l]);
-                }
-                l++;
-            }else break;
+        List<Integer> ans = new ArrayList<>();
+        if (m == null || m.length == 0) {
+            return ans;
         }
-        return ll;
+
+        int row = m.length;
+        int col = m[0].length;
+        int left = 0;
+        int right = col - 1;
+        int top = 0;
+        int bottom = row - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right across the top row
+            for (int i = left; i <= right; i++) {
+                ans.add(m[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom down the right column
+            for (int i = top; i <= bottom; i++) {
+                ans.add(m[i][right]);
+            }
+            right--;
+
+            // Check if there are rows left to traverse
+            if (top <= bottom) {
+                // Traverse from right to left across the bottom row
+                for (int i = right; i >= left; i--) {
+                    ans.add(m[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Check if there are columns left to traverse
+            if (left <= right) {
+                // Traverse from bottom to top up the left column
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(m[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
     }
 }
+
+        // int l=0,r=m[0].length-1,u=0,d=m.length-1;
+        // List<Integer> ll=new ArrayList<>();
+        // while(true){
+        //     if(l<=r){
+        //         for(int i=l;i<=r;++i){
+        //             ll.add(m[u][i]);
+        //         }
+        //         u++;
+        //     }else break;
+        //     if(u<=d){
+        //         for(int i=u;i<=d;++i){
+        //             ll.add(m[i][r]);
+        //         }
+        //         r--;
+        //     }else break;
+        //     if(l<=r){
+        //         for(int i=r;i>=l;--i){
+        //             ll.add(m[d][i]);
+        //         }
+        //         d--;
+        //     }else break;
+        //     if(u<=d){
+        //         for(int i=d;i>=u;--i){
+        //             ll.add(m[i][l]);
+        //         }
+        //         l++;
+        //     }else break;
+//         }
+//         return ll;
+//     }
+// }
