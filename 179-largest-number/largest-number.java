4688c118007b40abba3b83eb46e5399aa25bccee
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-09-18 16:27:34 +0530

    Time: 6 ms (54.52%) | Memory: 42.8 MB (92.40%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        int sum = 0;
+        for(int i : nums){
+            sum += i;
+        }
+        if(sum == 0)return "0";
+        String arr[] = new String[nums.length];
+        int k=0;
+        for(int i : nums){
+            arr[k++] = i+"";
+        }
+        int n = nums.length;
+        Arrays.sort(arr,new Comparator<String>(){
+            @Override
+            public int compare(String x,String y){
+                String xy = x+y;
+                String yx = y+x;
+                return yx.compareTo(xy);
+            }
+        });
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i < n ; i++)sb.append(arr[i]);
+        
+        return sb+"";
+    }
+}
