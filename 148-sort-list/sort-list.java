e9b38119c3226cebe4d52503e076422efce20dd6
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-10-19 14:13:40 +0530

    Time: 10 ms (49.94%) | Memory: 57.7 MB (5.66%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -8,49 +8,102 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-  
+//FIRST APPROACH
+class Solution {
   public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
+     if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Get the midpoint and split the list
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head); // Sort the left half
+        ListNode right = sortList(mid);  // Sort the right half
+
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+
+    // Merge two sorted linked lists
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dh = new ListNode(0); // Dummy head
+        ListNode tail = dh;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next; // Move tail
+        }
         
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
+        // Attach the remaining elements
+        tail.next = (list1 != null) ? list1 : list2;
+        return dh.next; // Return the merged list, skipping the dummy head
     }
-    
-    prev.next = null;
-    
 
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
-    return merge(l1, l2);
-  }
-  
-  ListNode merge(ListNode l1, ListNode l2) {
-    ListNode l = new ListNode(0), p = l;
-    
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
-        l1 = l1.next;
-      } else {
-        p.next = l2;
-        l2 = l2.next;
-      }
-      p = p.next;
+    // Find the midpoint of the list using the slow and fast pointer approach
+    ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        
+        ListNode mid = midPrev.next; // Mid is the next of midPrev
+        midPrev.next = null; // Split the list into two halves
+        return mid;
     }
-    
-    if (l1 != null)
-      p.next = l1;
-    
-    if (l2 != null)
-      p.next = l2;
-    
-    return l.next;
-  }
-
 }
+
+
+// SECOND APPROACH  
+// class Solution {
+  
+//   public ListNode sortList(ListNode head) {
+//     if (head == null || head.next == null)
+//       return head;
+        
+//     ListNode prev = null, slow = head, fast = head;
+    
+//     while (fast != null && fast.next != null) {
+//       prev = slow;
+//       slow = slow.next;
+//       fast = fast.next.next;
+//     }
+    
+//     prev.next = null;
+    
+
+//     ListNode l1 = sortList(head);
+//     ListNode l2 = sortList(slow);
+//     return merge(l1, l2);
+//   }
+  
+//   ListNode merge(ListNode l1, ListNode l2) {
+//     ListNode l = new ListNode(0), p = l;
+    
+//     while (l1 != null && l2 != null) {
+//       if (l1.val < l2.val) {
+//         p.next = l1;
+//         l1 = l1.next;
+//       } else {
+//         p.next = l2;
+//         l2 = l2.next;
+//       }
+//       p = p.next;
+//     }
+    
+//     if (l1 != null)
+//       p.next = l1;
+    
+//     if (l2 != null)
+//       p.next = l2;
+    
+//     return l.next;
+//   }
+
+// }
