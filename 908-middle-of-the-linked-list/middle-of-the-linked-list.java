ddb0769d4d661734950648e062632816696cfc5b
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-10-19 13:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (46.44%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,20 +10,36 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
 
-        ListNode fast = head;
-        ListNode slow = head;
+//FIRST APPROACH
+           ListNode s = head;
+           ListNode f = head;
 
-        while (fast != null) {
-            fast = fast.next;
-            if (fast != null) {
-                fast = fast.next;
-                slow = slow.next;
-            }
-        }
-
-        return slow;
+           while(f!= null && f.next != null){
+            s = s.next;
+            f = f.next.next;
+           }        
+           return s;
     }
 }
+
+// SECOND APPROACH 
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         if (head == null || head.next == null)
+//             return head;
+
+//         ListNode fast = head;
+//         ListNode slow = head;
+
+//         while (fast != null) {
+//             fast = fast.next;
+//             if (fast != null) {
+//                 fast = fast.next;
+//                 slow = slow.next;
+//             }
+//         }
+
+//         return slow;
+//     }
+// }
