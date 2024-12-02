f742841384ed4d9a2a6cdd3c176bc7939a95a2dc
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2024-12-02 15:11:52 +0530

    Time: 589 ms (27.36%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-#same table weather named as w1 and w2 where we are checking the date difference like the consecutive dates like 1, 2 using dateddiff() where we check w2 and w2 the record date and then  check for the next condition which says temp of day2 should be > temp of day 1
-select w1.id
-from Weather w1, Weather w2
-where datediff(w1.recordDate, w2.recordDate)=1 and w1.temperature>w2.temperature;
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
