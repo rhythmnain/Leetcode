2d7b996f386b854e4a922c9d7d1813bcf92a08c0
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-04-28 20:16:53 +0530

    Create file

---
+++ 2302-CountSubarraysWithScoreLessThanK/version/java/2302-CountSubarraysWithScoreLessThanK_20250428_201653.java
@@ -0,0 +1,23 @@
+// Last updated: 4/28/2025, 8:16:53 PM
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int start = 0;
+        long sum = 0;
+        long count = 0;
+        
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            
+            // Shrink the window if the score exceeds or equals k
+            while (sum * (end - start + 1) >= k) {
+                sum -= nums[start];
+                start++;
+            }
+            
+            // Add the number of valid subarrays ending at 'end'
+            count += (end - start + 1);
+        }
+        
+        return count;
+    }
+}
