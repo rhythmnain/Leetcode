3e1a70d75a1d9ce8804e5e3ef4128bb6ff07d446
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-05-20 20:29:36 +0530

    Create file

---
+++ 3355-ZeroArrayTransformationI/version/java/3355-ZeroArrayTransformationI_20250520_202936.java
@@ -0,0 +1,24 @@
+// Last updated: 5/20/2025, 8:29:36 PM
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+    int n = nums.length;
+    int diff[] = new int[n+1];
+    for(int d[] : queries)
+    {
+     diff[d[0]] += 1;
+     diff[d[1]+1] -= 1;
+    }
+
+    for(int i=1; i<=n; i++)
+    diff[i] += diff[i-1];
+    
+    for(int i=0; i<n; i++)
+    {
+        if(nums[i]>diff[i])
+        return false;
+    }
+    
+    return true;
+
+    }
+}
