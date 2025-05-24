91c576c129db0aaf9d1f5c29497ef8f3b9823704
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-05-24 14:43:21 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/version/java/2942-FindWordsContainingCharacter_20250524_144321.java
@@ -0,0 +1,14 @@
+// Last updated: 5/24/2025, 2:43:21 PM
+import java.util.*;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
