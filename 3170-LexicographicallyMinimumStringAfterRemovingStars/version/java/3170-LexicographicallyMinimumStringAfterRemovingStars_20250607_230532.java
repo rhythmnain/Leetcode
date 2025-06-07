e7d4da2df3a541016f4b0f48ba43af0cb991f42f
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-07 23:05:32 +0530

    Create 3170-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3170-LexicographicallyMinimumStringAfterRemovingStars/version/java/3170-LexicographicallyMinimumStringAfterRemovingStars_20250607_230532.java
@@ -0,0 +1,35 @@
+// Last updated: 6/7/2025, 11:05:32 PM
+class Solution {
+    public String clearStars(String s) {
+        List<Deque<Integer>> freq = new ArrayList<>();
+        for (int i = 0; i < 26; i++) freq.add(new ArrayDeque<>());
+
+        int a = 'a';
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '*') {
+                for (int j = 0; j < 26; j++) {
+                    if (!freq.get(j).isEmpty()) {
+                        freq.get(j).removeLast();
+                        break;
+                    }
+                }
+            } else {
+                freq.get(s.charAt(i) - a).add(i);
+            }
+        }
+
+        boolean[] keep = new boolean[s.length()];
+        for (Deque<Integer> stack : freq) {
+            for (int idx : stack) {
+                keep[idx] = true;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (keep[i]) result.append(s.charAt(i));
+        }
+        return result.toString();
+    }
+}
