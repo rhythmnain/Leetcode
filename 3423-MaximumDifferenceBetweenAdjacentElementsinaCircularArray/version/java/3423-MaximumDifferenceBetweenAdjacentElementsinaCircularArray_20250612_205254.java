d6ff3f60f58b9fcd0ea8076bb6c5a47397723b0b
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-12 20:52:53 +0530

    Create 3423-MaximumDifferenceBetweenAdjacentElementsinaCircularArray

---
+++ 3423-MaximumDifferenceBetweenAdjacentElementsinaCircularArray/version/java/3423-MaximumDifferenceBetweenAdjacentElementsinaCircularArray_20250612_205254.java
@@ -0,0 +1,10 @@
+// Last updated: 6/12/2025, 8:52:54 PM
+class Solution {
+    public int maxAdjacentDistance(int[] nums) {
+        int maxi = 0;
+        for (int i = 0; i < nums.length - 1; i++)
+            maxi = Math.max(maxi, Math.abs(nums[i + 1] - nums[i]));
+        maxi = Math.max(maxi, Math.abs(nums[nums.length - 1] - nums[0]));
+        return maxi;
+    }
+}
