4a63c65ca4e77be3d924e5b4416986ec10bed0c8
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-16 19:45:14 +0530

    Create 2016-MaximumDifferenceBetweenIncreasingElements

---
+++ 2016-MaximumDifferenceBetweenIncreasingElements/version/java/2016-MaximumDifferenceBetweenIncreasingElements_20250616_194514.java
@@ -0,0 +1,17 @@
+// Last updated: 6/16/2025, 7:45:14 PM
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int minVal = nums[0];
+        int maxDiff = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > minVal) {
+                maxDiff = Math.max(maxDiff, nums[i] - minVal);
+            } else {
+                minVal = nums[i];
+            }
+        }
+
+        return maxDiff;
+    }
+}
