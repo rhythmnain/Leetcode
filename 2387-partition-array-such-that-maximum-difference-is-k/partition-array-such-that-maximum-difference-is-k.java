e4170c74183ac48a73e3ba637fe21148a6bb2963
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-19 21:08:29 +0530

    Time: 34 ms (42.55%) | Memory: 57 MB (54.61%) - LeetSync

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);  // Sort the array
+        int count = 1;       // At least one group is needed
+        int start = nums[0]; // Start of current group
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - start > k) {
+                count++;        // Need a new group
+                start = nums[i]; // Reset start of the new group
+            }
+        }
+        
+        return count;
+    }
+}
