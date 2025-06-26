df7ad9022627d2c6446d6546ac789080e03fd7a3
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-26 20:06:24 +0530

    Create 2311-LongestBinarySubsequenceLessThanorEqualtoK

---
+++ 2311-LongestBinarySubsequenceLessThanorEqualtoK/version/java/2311-LongestBinarySubsequenceLessThanorEqualtoK_20250626_200625.java
@@ -0,0 +1,21 @@
+// Last updated: 6/26/2025, 8:06:25 PM
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        int sum = 0;  // \U0001f389 Our piggy bank, starting empty! Will hold the decimal value. \U0001f437
+        int len = 0; // \U0001f4cf Counter for the candy line's length!
+
+        int bits = (int)(Math.log(k) / Math.log(2)) + 1; 
+
+        for (int i = s.length() - 1; i >= 0; i--) { 
+
+            char c = s.charAt(i); 
+
+            len += (c == '0') ? 1 : 
+                    (i < s.length() - bits || sum + (1 << (s.length() - 1 - i)) > k) ? 0 :  
+                    ((sum += 1 << (s.length() - 1 - i)) > -1 ? 1 : 0);  
+
+        }
+
+        return len; // \U0001f381 The longest candy line we could make!
+    }
+}
