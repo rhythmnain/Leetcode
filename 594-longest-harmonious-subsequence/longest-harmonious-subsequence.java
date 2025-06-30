481873caf56ab0240a4d39e7e9843caee57810d0
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-06-30 21:01:32 +0530

    Time: 17 ms (37.03%) | Memory: 45.2 MB (94.01%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        // Count the frequency of each number
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxLength = 0;
+
+        // Check for each key if key + 1 exists
+        for (int key : freqMap.keySet()) {
+            if (freqMap.containsKey(key + 1)) {
+                int length = freqMap.get(key) + freqMap.get(key + 1);
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+
+        return maxLength;
+    }
+}
