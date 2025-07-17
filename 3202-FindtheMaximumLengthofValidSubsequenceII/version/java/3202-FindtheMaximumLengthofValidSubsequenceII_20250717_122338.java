bce70c5037b97d099199e65a8bc4dab0227b9c67
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-07-17 12:23:37 +0530

    Create 3202-FindtheMaximumLengthofValidSubsequenceII

---
+++ 3202-FindtheMaximumLengthofValidSubsequenceII/version/java/3202-FindtheMaximumLengthofValidSubsequenceII_20250717_122338.java
@@ -0,0 +1,20 @@
+// Last updated: 7/17/2025, 12:23:38 PM
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int res = 2;
+        for (int j = 0; j < k; j++) {
+            int[] dp = new int[k];
+
+            for (int i = 0; i < nums.length; i++) {
+                int mod = nums[i] % k;
+                int pos = (j - mod + k) % k;
+                dp[mod] = dp[pos] + 1;
+            }
+
+            for (int val : dp) {
+                res = Math.max(res, val);
+            }
+        }
+        return res;
+    }
+}
