f95f10db0ac3cfaeedcb862f5138f359666e50c4
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-07-19 23:01:11 +0530

    Create 1233-RemoveSub-FoldersfromtheFilesystem

---
+++ 1233-RemoveSub-FoldersfromtheFilesystem/version/java/1233-RemoveSub-FoldersfromtheFilesystem_20250719_230111.java
@@ -0,0 +1,71 @@
+// Last updated: 7/19/2025, 11:01:11 PM
+class Solution {
+class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            this.children = new TrieNode[27];
+            this.isEndOfWord = false;
+        }
+    }
+
+    TrieNode root;
+
+    private int getIndex(char ch) {
+        if (ch == '/') {
+            return 26;
+        }
+
+        return ch - 'a';
+    }
+
+    private void insert(String s) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = getIndex(s.charAt(i));
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.children[26] = new TrieNode();
+        curr = curr.children[26];
+        curr.isEndOfWord = true;
+    }
+
+    private boolean search(String s) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (curr.isEndOfWord) {
+                return true;
+            }
+
+            int idx = getIndex(s.charAt(i));
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.isEndOfWord;
+    }
+
+    public List<String> removeSubfolders(String[] folder) {
+        root = new TrieNode();
+        Arrays.sort(folder, (a, b) -> a.length() - b.length());
+        List<String> ans = new ArrayList<>();
+
+        for (String f : folder) {
+            if (!search(f)) {
+                insert(f);
+                ans.add(f);
+            }
+        }
+        return ans;
+    }
+}
