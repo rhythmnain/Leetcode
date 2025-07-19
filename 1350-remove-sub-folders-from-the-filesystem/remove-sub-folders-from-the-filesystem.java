f712a17e40badbab2abf71855a34e8fea3fccd39
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-07-19 22:58:59 +0530

    Time: 19 ms (100.00%) | Memory: 55.4 MB (74.41%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,18 +1,70 @@
 class Solution {
+class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            this.children = new TrieNode[27];
+            this.isEndOfWord = false;
+        }
+    }
+
+    TrieNode root;
+
+    private int getIndex(char ch) {
+        if (ch == '/') {
+            return 26;
+        }
+
+        return ch - 'a';
+    }
+
+    private void insert(String s) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = getIndex(s.charAt(i));
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.children[26] = new TrieNode();
+        curr = curr.children[26];
+        curr.isEndOfWord = true;
+    }
+
+    private boolean search(String s) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (curr.isEndOfWord) {
+                return true;
+            }
+
+            int idx = getIndex(s.charAt(i));
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.isEndOfWord;
+    }
+
     public List<String> removeSubfolders(String[] folder) {
-        Arrays.sort(folder);
-        
+        root = new TrieNode();
+        Arrays.sort(folder, (a, b) -> a.length() - b.length());
         List<String> ans = new ArrayList<>();
-        ans.add(folder[0]);
-        
-        for (int i = 1; i < folder.length; i++) {
-            String lastFolder = ans.get(ans.size() - 1) + "/";
-            
-            if (!folder[i].startsWith(lastFolder)) {
-                ans.add(folder[i]);
+
+        for (String f : folder) {
+            if (!search(f)) {
+                insert(f);
+                ans.add(f);
             }
         }
-        
         return ans;
     }
 }
