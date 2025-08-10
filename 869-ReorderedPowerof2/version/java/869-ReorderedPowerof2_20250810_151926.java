b3cca537a1ed1a6429b0d90817c6eb3fd4796d7d
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-10 15:19:26 +0530

    Create 869-ReorderedPowerof2

---
+++ 869-ReorderedPowerof2/version/java/869-ReorderedPowerof2_20250810_151926.java
@@ -0,0 +1,16 @@
+// Last updated: 8/10/2025, 3:19:26 PM
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String target = sortedString(n);
+        for (int i = 0; i < 31; i++) {
+            if (sortedString(1 << i).equals(target)) return true;
+        }
+        return false;
+    }
+
+    private String sortedString(int x) {
+        char[] arr = String.valueOf(x).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
