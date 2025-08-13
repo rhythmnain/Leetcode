f1c2c4252c0452661d9ab2176cde3f1ed0f66b96
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-13 20:39:19 +0530

    Create 326-PowerofThree

---
+++ 326-PowerofThree/version/java/326-PowerofThree_20250813_203921.java
@@ -0,0 +1,7 @@
+// Last updated: 8/13/2025, 8:39:21 PM
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOf3 % n == 0;
+    }
+}
