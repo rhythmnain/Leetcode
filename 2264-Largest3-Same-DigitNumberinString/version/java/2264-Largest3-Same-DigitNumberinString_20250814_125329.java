a536b91b4f9a404fd69da0a5d58dfce0807d9a83
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-14 12:53:27 +0530

    Create 2264-Largest3-Same-DigitNumberinString

---
+++ 2264-Largest3-Same-DigitNumberinString/version/java/2264-Largest3-Same-DigitNumberinString_20250814_125329.java
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2025, 12:53:29 PM
+class Solution {
+    public String largestGoodInteger(String num) {
+        String ans = ""; 
+        for (int i = 0; i + 2 < num.length(); i++) {
+            // Check if three consecutive digits are same
+            if (num.charAt(i) == num.charAt(i + 1) && num.charAt(i) == num.charAt(i + 2)) {
+                String triplet = num.substring(i, i + 3); // Get substring of length 3
+                if (triplet.compareTo(ans) > 0) { // Compare lexicographically
+                    ans = triplet;
+                }
+            }
+        }
+        return ans;
+    }
+}
