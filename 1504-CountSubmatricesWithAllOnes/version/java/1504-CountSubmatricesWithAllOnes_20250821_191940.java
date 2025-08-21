9ea66a30ce591eda3a43011de50cb12a13c2016a
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-21 19:19:37 +0530

    Create 1504-CountSubmatricesWithAllOnes

---
+++ 1504-CountSubmatricesWithAllOnes/version/java/1504-CountSubmatricesWithAllOnes_20250821_191940.java
@@ -0,0 +1,23 @@
+// Last updated: 8/21/2025, 7:19:40 PM
+class Solution {
+    public int numSubmat(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] h = new int[n];
+        int cnt = 0;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                h[j] = mat[i][j] == 1 ? h[j] + 1 : 0;
+            }
+
+            for(int j=0; j<n; j++) {
+                int mn = h[j];
+                for(int k=j; k>=0 && mn>0; k--) {
+                    mn = Math.min(mn, h[k]);
+                    cnt += mn;
+                }
+            }
+        }
+        return cnt;
+    }
+}
