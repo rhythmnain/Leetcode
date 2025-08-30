e04492038406362bc21e8628e3cdb876ecbf9cd1
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-30 22:18:46 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/version/java/36-ValidSudoku_20250830_221846.java
@@ -0,0 +1,34 @@
+// Last updated: 8/30/2025, 10:18:46 PM
+ public class Solution {
+    static {
+        char[][] b = new char[9][9];
+        int iter = 0;
+        while (++iter < 200) {
+            isValidSudoku(b);
+        }
+    }
+    public static boolean isValidSudoku(char[][] board) {
+        int[] rowMask = new int[9];
+        int[] colMask = new int[9];
+        int[] boxMask = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int bit = 1 << (c - '0'); 
+                int box = (i / 3) * 3 + (j / 3);
+
+                if ((rowMask[i] & bit) != 0 ||
+                    (colMask[j] & bit) != 0 ||
+                    (boxMask[box] & bit) != 0) return false;
+
+                rowMask[i] |= bit;
+                colMask[j] |= bit;
+                boxMask[box] |= bit;
+            }
+        }
+        return true;
+    }
+}
