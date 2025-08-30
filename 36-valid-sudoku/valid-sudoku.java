25b33c9fabf2769ead0f9540625d09331f2f7028
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-08-30 22:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (89.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,54 +1,31 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        //O(1) time and space
-        for(int i = 0; i < 9; i++){
-            if (!isValidRow(board, i) || 
-                !isValidCol(board, i) || 
-                !isValidBox(board, i)) 
-                    return false;            
+ public class Solution {
+    static {
+        char[][] b = new char[9][9];
+        int iter = 0;
+        while (++iter < 200) {
+            isValidSudoku(b);
         }
-        return true;
     }
+    public static boolean isValidSudoku(char[][] board) {
+        int[] rowMask = new int[9];
+        int[] colMask = new int[9];
+        int[] boxMask = new int[9];
 
-    public boolean isValidRow(char[][] board, int row) {
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for(int j = 0; j < 9; j++){
-            char currChar = board[row][j];
-            if(currChar != '.'){
-                mp.put(currChar, mp.getOrDefault(currChar, 0)+1);
-                if (mp.get(currChar) > 1) 
-                    return false;
-            }
-        }
-        return true;
-    }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
 
-    public boolean isValidCol(char[][] board, int col) {
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for(int i = 0; i < 9; i++){
-            char currChar = board[i][col];
-            if(currChar != '.'){
-                mp.put(currChar, mp.getOrDefault(currChar, 0)+1);
-                if (mp.get(currChar) > 1) 
-                    return false;
-            }
-        }
-        return true;
-    }
+                int bit = 1 << (c - '0'); 
+                int box = (i / 3) * 3 + (j / 3);
 
-    public boolean isValidBox(char[][] board, int box) {
-        HashMap<Character, Integer> mp = new HashMap<>();
-        int startRow = 3 * (box / 3);
-        int startCol = 3 * (box % 3);
+                if ((rowMask[i] & bit) != 0 ||
+                    (colMask[j] & bit) != 0 ||
+                    (boxMask[box] & bit) != 0) return false;
 
-        for (int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++){
-                char currChar = board[startRow+i][startCol+j];
-                if(currChar != '.'){
-                    mp.put(currChar, mp.getOrDefault(currChar, 0)+1);
-                    if (mp.get(currChar) > 1) 
-                        return false;
-                }
+                rowMask[i] |= bit;
+                colMask[j] |= bit;
+                boxMask[box] |= bit;
             }
         }
         return true;
