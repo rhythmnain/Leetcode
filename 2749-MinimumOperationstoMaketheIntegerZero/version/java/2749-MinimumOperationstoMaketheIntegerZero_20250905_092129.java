5617a8077d804d31140c4b43b3661275ae2ff4ed
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-09-05 09:21:26 +0530

    Create 2749-MinimumOperationstoMaketheIntegerZero

---
+++ 2749-MinimumOperationstoMaketheIntegerZero/version/java/2749-MinimumOperationstoMaketheIntegerZero_20250905_092129.java
@@ -0,0 +1,13 @@
+// Last updated: 9/5/2025, 9:21:29 AM
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        for (int t = 0; t <= 60; t++) {
+            long s = (long) num1 - (long) t * num2;
+            if (s < 0) continue;
+            if (s < t) continue;
+            int ones = Long.bitCount(s);
+            if (ones <= t) return t;
+        }
+        return -1;
+    }
+}
