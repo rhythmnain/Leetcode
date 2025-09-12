4a938ea8a56c7cac31cb914c3d3ab8760b4deb88
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-09-12 18:55:13 +0530

    Create 3227-VowelsGameinaString

---
+++ 3227-VowelsGameinaString/version/java/3227-VowelsGameinaString_20250912_185512.java
@@ -0,0 +1,9 @@
+// Last updated: 9/12/2025, 6:55:12 PM
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if ((0x104111 >> (s.charAt(i) - 97) & 1) != 0)
+                return true;
+        return false;
+    }
+}
