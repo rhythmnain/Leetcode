d283916f1a6f5dbc6e946a11ef4cc3ea03e332da
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-09-25 17:19:27 +0530

    Create 120-Triangle

---
+++ 120-Triangle/version/java/120-Triangle_20250925_171926.java
@@ -0,0 +1,27 @@
+// Last updated: 9/25/2025, 5:19:26 PM
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        
+        // Use an array to store the minimum path sum for each position
+        int[] dp = new int[n];
+        
+        // Initialize dp array with the last row of the triangle
+        List<Integer> lastRow = triangle.get(n - 1);
+        for (int i = 0; i < n; i++) {
+            dp[i] = lastRow.get(i);
+        }
+        
+        // Traverse the triangle from bottom to top
+        for (int row = n - 2; row >= 0; row--) {
+            List<Integer> currentRow = triangle.get(row);
+            for (int col = 0; col < currentRow.size(); col++) {
+                // Update dp array with the minimum path sum for each position
+                dp[col] = currentRow.get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+        
+        // The first element of dp array will contain the minimum path sum from top to bottom
+        return dp[0];
+    }
+}
