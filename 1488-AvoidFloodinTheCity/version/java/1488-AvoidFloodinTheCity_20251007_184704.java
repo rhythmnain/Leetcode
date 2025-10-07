7806ca9e2404f1c0e6b9d2ce759dd34f8ab05e9e
Rhythm Nain Kour <118470834+rhythmnain@users.noreply.github.com>
2025-10-07 18:47:03 +0530

    Create 1488-AvoidFloodinTheCity

---
+++ 1488-AvoidFloodinTheCity/version/java/1488-AvoidFloodinTheCity_20251007_184704.java
@@ -0,0 +1,27 @@
+// Last updated: 10/7/2025, 6:47:04 PM
+class Solution {
+
+    public int[] avoidFlood(int[] rains) {
+        int[] ans = new int[rains.length];
+        Arrays.fill(ans, 1);
+        TreeSet<Integer> st = new TreeSet<Integer>();
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < rains.length; ++i) {
+            if (rains[i] == 0) {
+                st.add(i);
+            } else {
+                ans[i] = -1;
+                if (mp.containsKey(rains[i])) {
+                    Integer it = st.ceiling(mp.get(rains[i]));
+                    if (it == null) {
+                        return new int[0];
+                    }
+                    ans[it] = rains[i];
+                    st.remove(it);
+                }
+                mp.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
